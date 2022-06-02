cf1fe6aaab367236c757a1cf9f0702dacae84307
Roshan Swain <swainroshan001@gmail.com>
2022-06-02 20:56:43 +0530

    added math director

---
+++ Math/_matrix_exponentiation.cpp
@@ -0,0 +1,17 @@
+/*
+Matrix Exponentiation
+
+Essence:
+To use technique of "Matrix Exponentiation" to solve specific set of problems where you can easily derive a recursive relation.
+
+Prequisites:
+1. Given Two Matrices, find their product.
+2. Given product of matrix, and one matrix, find the other.
+3. Given matrix of size dxd, find its n'th power in
+O(d^3log(n)).
+
+Patterns:
+
+
+
+*/
